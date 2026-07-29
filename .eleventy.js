603e2f2cb7fd65103c28bc35c0f2c5e397994e18
Shannon Field <110503192+shannonjfield94@users.noreply.github.com>
2026-07-29 08:49:53 -0400

    Add CNAME and Pages deployment workflow

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/assets/favicons");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addFilter("markdownify", (content) => markdownIt.renderInline(content));
 
   eleventyConfig.addFilter("byGeneration", (people, gen) => {
